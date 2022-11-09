730a57d15a5e500fcd77274b32ab6999e036e2cf
Alexandre Jaloto <alexandrejaloto@gmail.com>
2022-11-09 15:15:53 -0300

    run in other computer

--- R/simulation.R
+++ R/simulation.R
@@ -18,11 +18,159 @@ load('rdata/samples.RData')
 load('rdata/official_constants.RData')
 
 areas <- c('CH', 'CN', 'LC', 'MT')
-replications <- 20
+areas <- c('CH', 'CN', 'MT')
+areas <- c('CN')
+replications <- 1
 
 # end
 
-# selection: random; fix length (45) ----
+# selection: random; fixed length (45) ----
+
+for (area_ in areas)
+{
+  # area_ <- 'CN'
+  load(paste0('rdata/resps_', area_, '.RData'))
+
+  start.theta <- (mean(real[[area_]]) - official.constants[[area_]]$m)/official.constants[[area_]]$s
+
+  items <- subset (pars, area == area_)
+
+  results <- list()
+
+  for (rep in 1:replications)
+  {
+    set.seed(rep)
+    results[[rep]] <- simCAT::simCAT(
+      resps = resps[[rep]][1:10,],
+      bank = items[,1:3],
+      start.theta = start.theta,
+      sel.method = 'random',
+      cat.type = 'fixed',
+      acceleration = 1,
+      threshold = 45,
+      rmax = 1,
+      content.names = 1:30,
+      content.props = rep(1/30, 30),
+      content.items = items$CO_HABILIDADE,
+      met.content = "MCCAT",
+      stop = list(fixed = 45)
+    )
+  }
+  save(results, file = paste0('results/ALETF452_', area_, '.RData'))
+
+}
+
+
+
+# selection: random; fixed length (20) ----
+
+for (area_ in areas)
+{
+  load(paste0('rdata/resps_', area_, '.RData'))
+
+  start.theta <- (mean(real[[area_]]) - official.constants[[area_]]$m)/official.constants[[area_]]$s
+
+  items <- subset (pars, area == area_) %>%
+    select (NU_PARAM_A, NU_PARAM_B, NU_PARAM_C)
+
+  results <- list()
+
+  for (rep in 1:replications)
+  {
+    set.seed(rep)
+    results[[rep]] <- simCAT::simCAT(
+      resps = resps[[rep]],
+      bank = items[,1:3],
+      start.theta = start.theta,
+      sel.method = 'random',
+      cat.type = 'fixed',
+      acceleration = 1,
+      threshold = 20,
+      rmax = 1,
+      content.names = 1:30,
+      content.props = rep(1/30, 30),
+      content.items = items$CO_HABILIDADE,
+      met.content = "MCCAT",
+      stop = list(fixed = 20)
+    )
+  }
+
+  save(results, file = paste0('results/ALETF20_', area_, '.RData'))
+}
+
+# selection: random; standard error (0.3) ----
+
+for (area_ in areas)
+{
+  load(paste0('rdata/resps_', area_, '.RData'))
+
+  start.theta <- (mean(real[[area_]]) - official.constants[[area_]]$m)/official.constants[[area_]]$s
+
+  items <- subset (pars, area == area_) %>%
+    select (NU_PARAM_A, NU_PARAM_B, NU_PARAM_C)
+
+  results <- list()
+
+  for (rep in 1:replications)
+  {
+    set.seed(rep)
+    results[[rep]] <- simCAT::simCAT(
+      resps = resps[[rep]],
+      bank = items[,1:3],
+      start.theta = start.theta,
+      sel.method = 'random',
+      cat.type = 'variable',
+      acceleration = 1,
+      threshold = .3,
+      rmax = 1,
+      content.names = 1:30,
+      content.props = rep(1/30, 30),
+      content.items = items$CO_HABILIDADE,
+      met.content = "MCCAT",
+      stop = list(se = .3, min.items = 15, max.items = 60)
+    )
+  }
+
+  save(results, file = paste0('results/ALEEP30_', area_, '.RData'))
+}
+
+# selection: random; standard error (.3)/error reduction (.015) ----
+
+for (area_ in areas)
+{
+  load(paste0('rdata/resps_', area_, '.RData'))
+
+  start.theta <- (mean(real[[area_]]) - official.constants[[area_]]$m)/official.constants[[area_]]$s
+
+  items <- subset (pars, area == area_) %>%
+    select (NU_PARAM_A, NU_PARAM_B, NU_PARAM_C)
+
+  results <- list()
+
+  for (rep in 1:replications)
+  {
+    set.seed(rep)
+    results[[rep]] <- simCAT::simCAT(
+      resps = resps[[rep]],
+      bank = items[,1:3],
+      start.theta = start.theta,
+      sel.method = 'random',
+      cat.type = 'variable',
+      acceleration = 1,
+      threshold = .3,
+      rmax = 1,
+      content.names = 1:30,
+      content.props = rep(1/30, 30),
+      content.items = items$CO_HABILIDADE,
+      met.content = "MCCAT",
+      stop = list(se = .3, min.items = 15, max.items = 60, hypo = .015, hyper = Inf)
+    )
+  }
+
+  save(results, file = paste0('results/ALEEP30RE015_', area_, '.RData'))
+}
+
+# selection: MFI; fixed length (45) ----
 
 for (area_ in areas)
 {
@@ -42,7 +190,7 @@ for (area_ in areas)
       resps = resps[[rep]],
       bank = items[,1:3],
       start.theta = start.theta,
-      sel.method = 'random',
+      sel.method = 'MFI',
       cat.type = 'fixed',
       acceleration = 1,
       threshold = 45,
@@ -54,118 +202,13 @@ for (area_ in areas)
       stop = list(fixed = 45)
     )
   }
-  save(results, file = paste0('results/ALETF45_', area_, '.RData'))
-
-}
-
-
-
-# selection: random; fix length (20) ----
-
-for (area_ in areas)
-{
-  load(paste0('rdata/resps_', area_, '.RData'))
-
-  start.theta <- (mean(real[[area_]]) - official.constants[[area_]]$m)/official.constants[[area_]]$s
-
-  items <- subset (pars, area == area_) %>%
-    select (NU_PARAM_A, NU_PARAM_B, NU_PARAM_C)
-
-  results <- list()
-
-  set.seed(1000)
-  for (rep in 1:replications)
-    results[[rep]] <- simCAT::simCAT(
-      resps = resps[[rep]],
-      bank = items,
-      start.theta = start.theta,
-      sel.method = 'random',
-      stop = list(fixed = 20)
-    )
-
-  save(results, file = paste0('results/ALETF20_', area_, '.RData'))
-}
-
-# selection: random; standard error (0.3) ----
-
-for (area_ in areas)
-{
-  load(paste0('rdata/resps_', area_, '.RData'))
-
-  start.theta <- (mean(real[[area_]]) - official.constants[[area_]]$m)/official.constants[[area_]]$s
-
-  items <- subset (pars, area == area_) %>%
-    select (NU_PARAM_A, NU_PARAM_B, NU_PARAM_C)
-
-  results <- list()
-
-  set.seed(1000)
-  for (rep in 1:replications)
-    results[[rep]] <- simCAT::simCAT(
-      resps = resps[[rep]],
-      bank = items,
-      start.theta = start.theta,
-      sel.method = 'random',
-      stop = list(se = .3, min.items = 15, max.items = 60)
-    )
-
-  save(results, file = paste0('results/ALEEP30_', area_, '.RData'))
-}
-
-# selection: random; standard error (.3)/error reduction (.015) ----
-
-for (area_ in areas)
-{
-  load(paste0('rdata/resps_', area_, '.RData'))
-
-  start.theta <- (mean(real[[area_]]) - official.constants[[area_]]$m)/official.constants[[area_]]$s
-
-  items <- subset (pars, area == area_) %>%
-    select (NU_PARAM_A, NU_PARAM_B, NU_PARAM_C)
-
-  results <- list()
-
-  set.seed(1000)
-  for (rep in 1:replications)
-    results[[rep]] <- simCAT::simCAT(
-      resps = resps[[rep]],
-      bank = items,
-      start.theta = start.theta,
-      sel.method = 'random',
-      stop = list(se = .3, min.items = 15, max.items = 60, hypo = .015, hyper = Inf)
-    )
-
-  save(results, file = paste0('results/ALEEP30RE015_', area_, '.RData'))
-}
-
-# selection: maximum Fisher information; fix length (45) ----
-
-for (area_ in areas)
-{
-  load(paste0('rdata/resps_', area_, '.RData'))
-
-  start.theta <- (mean(real[[area_]]) - official.constants[[area_]]$m)/official.constants[[area_]]$s
-
-  items <- subset (pars, area == area_) %>%
-    select (NU_PARAM_A, NU_PARAM_B, NU_PARAM_C)
-
-  results <- list()
-
-  set.seed(1000)
-  for (rep in 1:replications)
-    results[[rep]] <- simCAT::simCAT(
-      resps = resps[[rep]][1:10,],
-      bank = items,
-      start.theta = start.theta,
-      sel.method = 'MFI',
-      stop = list(fixed = 45)
-    )
-
   save(results, file = paste0('results/MIFTF45_', area_, '.RData'))
+
 }
 
 
-# selection: maximum Fisher information; fix length (20) ----
+
+# selection: MFI; fixed length (20) ----
 
 for (area_ in areas)
 {
@@ -178,20 +221,30 @@ for (area_ in areas)
 
   results <- list()
 
-  set.seed(1000)
   for (rep in 1:replications)
+  {
+    set.seed(rep)
     results[[rep]] <- simCAT::simCAT(
       resps = resps[[rep]],
-      bank = items,
+      bank = items[,1:3],
       start.theta = start.theta,
       sel.method = 'MFI',
+      cat.type = 'fixed',
+      acceleration = 1,
+      threshold = 20,
+      rmax = 1,
+      content.names = 1:30,
+      content.props = rep(1/30, 30),
+      content.items = items$CO_HABILIDADE,
+      met.content = "MCCAT",
       stop = list(fixed = 20)
     )
+  }
 
   save(results, file = paste0('results/MIFTF20_', area_, '.RData'))
 }
 
-# selection: maximum Fisher information; standard error (0.3) ----
+# selection: MFI; standard error (0.3) ----
 
 for (area_ in areas)
 {
@@ -204,20 +257,30 @@ for (area_ in areas)
 
   results <- list()
 
-  set.seed(1000)
   for (rep in 1:replications)
+  {
+    set.seed(rep)
     results[[rep]] <- simCAT::simCAT(
       resps = resps[[rep]],
-      bank = items,
+      bank = items[,1:3],
       start.theta = start.theta,
       sel.method = 'MFI',
+      cat.type = 'variable',
+      acceleration = 1,
+      threshold = .3,
+      rmax = 1,
+      content.names = 1:30,
+      content.props = rep(1/30, 30),
+      content.items = items$CO_HABILIDADE,
+      met.content = "MCCAT",
       stop = list(se = .3, min.items = 15, max.items = 60)
     )
+  }
 
   save(results, file = paste0('results/MIFEP30_', area_, '.RData'))
 }
 
-# selection: maximum Fisher information; standard error (.3)/error reduction (.015) ----
+# selection: MFI; standard error (.3)/error reduction (.015) ----
 
 for (area_ in areas)
 {
@@ -230,15 +293,171 @@ for (area_ in areas)
 
   results <- list()
 
-  set.seed(1000)
   for (rep in 1:replications)
+  {
+    set.seed(rep)
     results[[rep]] <- simCAT::simCAT(
       resps = resps[[rep]],
-      bank = items,
+      bank = items[,1:3],
       start.theta = start.theta,
       sel.method = 'MFI',
+      cat.type = 'variable',
+      acceleration = 1,
+      threshold = .3,
+      rmax = 1,
+      content.names = 1:30,
+      content.props = rep(1/30, 30),
+      content.items = items$CO_HABILIDADE,
+      met.content = "MCCAT",
       stop = list(se = .3, min.items = 15, max.items = 60, hypo = .015, hyper = Inf)
     )
+  }
+
+  save(results, file = paste0('results/MIFEP30RE015_', area_, '.RData'))
+}
+
+# selection: PR (acceleration parameter = -1); fixed length (45) ----
+
+for (area_ in areas)
+{
+  # area_ <- 'CH'
+  load(paste0('rdata/resps_', area_, '.RData'))
+
+  start.theta <- (mean(real[[area_]]) - official.constants[[area_]]$m)/official.constants[[area_]]$s
+
+  items <- subset (pars, area == area_)
+
+  results <- list()
+
+  for (rep in 1:replications)
+  {
+    set.seed(rep)
+    results[[rep]] <- simCAT::simCAT(
+      resps = resps[[rep]],
+      bank = items[,1:3],
+      start.theta = start.theta,
+      sel.method = 'progressive',
+      cat.type = 'fixed',
+      acceleration = -1,
+      threshold = 45,
+      rmax = .3,
+      content.names = 1:30,
+      content.props = rep(1/30, 30),
+      content.items = items$CO_HABILIDADE,
+      met.content = "MCCAT",
+      stop = list(fixed = 45)
+    )
+  }
+  save(results, file = paste0('results/PR-1TF45_', area_, '.RData'))
+
+}
+
+
+
+# selection: MFI; fixed length (20) ----
+
+for (area_ in areas)
+{
+  load(paste0('rdata/resps_', area_, '.RData'))
+
+  start.theta <- (mean(real[[area_]]) - official.constants[[area_]]$m)/official.constants[[area_]]$s
+
+  items <- subset (pars, area == area_) %>%
+    select (NU_PARAM_A, NU_PARAM_B, NU_PARAM_C)
+
+  results <- list()
+
+  for (rep in 1:replications)
+  {
+    set.seed(rep)
+    results[[rep]] <- simCAT::simCAT(
+      resps = resps[[rep]],
+      bank = items[,1:3],
+      start.theta = start.theta,
+      sel.method = 'MFI',
+      cat.type = 'fixed',
+      acceleration = 1,
+      threshold = 20,
+      rmax = 1,
+      content.names = 1:30,
+      content.props = rep(1/30, 30),
+      content.items = items$CO_HABILIDADE,
+      met.content = "MCCAT",
+      stop = list(fixed = 20)
+    )
+  }
+
+  save(results, file = paste0('results/MIFTF20_', area_, '.RData'))
+}
+
+# selection: MFI; standard error (0.3) ----
+
+for (area_ in areas)
+{
+  load(paste0('rdata/resps_', area_, '.RData'))
+
+  start.theta <- (mean(real[[area_]]) - official.constants[[area_]]$m)/official.constants[[area_]]$s
+
+  items <- subset (pars, area == area_) %>%
+    select (NU_PARAM_A, NU_PARAM_B, NU_PARAM_C)
+
+  results <- list()
+
+  for (rep in 1:replications)
+  {
+    set.seed(rep)
+    results[[rep]] <- simCAT::simCAT(
+      resps = resps[[rep]],
+      bank = items[,1:3],
+      start.theta = start.theta,
+      sel.method = 'MFI',
+      cat.type = 'variable',
+      acceleration = 1,
+      threshold = .3,
+      rmax = 1,
+      content.names = 1:30,
+      content.props = rep(1/30, 30),
+      content.items = items$CO_HABILIDADE,
+      met.content = "MCCAT",
+      stop = list(se = .3, min.items = 15, max.items = 60)
+    )
+  }
+
+  save(results, file = paste0('results/MIFEP30_', area_, '.RData'))
+}
+
+# selection: MFI; standard error (.3)/error reduction (.015) ----
+
+for (area_ in areas)
+{
+  load(paste0('rdata/resps_', area_, '.RData'))
+
+  start.theta <- (mean(real[[area_]]) - official.constants[[area_]]$m)/official.constants[[area_]]$s
+
+  items <- subset (pars, area == area_) %>%
+    select (NU_PARAM_A, NU_PARAM_B, NU_PARAM_C)
+
+  results <- list()
+
+  for (rep in 1:replications)
+  {
+    set.seed(rep)
+    results[[rep]] <- simCAT::simCAT(
+      resps = resps[[rep]],
+      bank = items[,1:3],
+      start.theta = start.theta,
+      sel.method = 'MFI',
+      cat.type = 'variable',
+      acceleration = 1,
+      threshold = .3,
+      rmax = 1,
+      content.names = 1:30,
+      content.props = rep(1/30, 30),
+      content.items = items$CO_HABILIDADE,
+      met.content = "MCCAT",
+      stop = list(se = .3, min.items = 15, max.items = 60, hypo = .015, hyper = Inf)
+    )
+  }
 
   save(results, file = paste0('results/MIFEP30RE015_', area_, '.RData'))
 }
